dc564a84fe24fcb63272725d86df5a88db82fa7f
mbertier <mbertier>
2004-03-13 01:35:01 +0000

    Comments update

--- microbuilder/html/install/language.php
+++ microbuilder/html/install/language.php
@@ -1,75 +1,76 @@
 <?php 
-// File: $Id: language.php,v 1.1 2004/03/01 10:09:14 mbertier Exp $ $Name:  $
-// ----------------------------------------------------------------------
-// POST-NUKE Content Management System
-// Copyright (C) 2001 by the Post-Nuke Development Team.
-// http://www.postnuke.com/
-// ----------------------------------------------------------------------
-// Based on:
-// PHP-NUKE Web Portal System - http://phpnuke.org/
-// Thatware - http://thatware.org/
-// ----------------------------------------------------------------------
-// LICENSE
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
-// Original Author of file: Gregor J. Rothfuss
-// Purpose of file: Provide ML functionality for the installer.
-// ----------------------------------------------------------------------
-/**
- * Loads the required language file for the installer
+/** Functions providing multilanguage functionality to the installer.
+ * @version      $Id: language.php,v 1.2 2004/03/13 01:35:01 mbertier Exp $
+ * @package      Installer
+ * @license      GPL
+ * @author       Gregor J. Rothfuss
  */
-function installer_get_language()
-{
+
+
+/** Loads the required language file for the installer. 
+ * @todo     Find out what is $language !
+ * @todo     Find out why postnuke UI strings have to be loaded
+*/
+function installer_get_language() {
     global $currentlang;
-    if (!isset($currentlang)) {
-        $currentlang = 'eng'; // english is the fallback
-    } 
+    
+    // English is default language
+    if ( ! isset($currentlang) ) $currentlang = 'eng';
+
+    // Loads strings for installer UI
     if (file_exists($file = "install/lang/$currentlang/global.php")) {
         @include $file;
     } elseif (file_exists($file = "install/lang/$language/global.php")) {
         @include $file;
     } 
+
+    // Loads strings for postnuke UI (?!)
     if (file_exists($file = "language/$currentlang/global.php")) {
         @include $file;
     } elseif (file_exists($file = "language/$language/global.php")) {
         @include $file;
     } 
 } 
-// Make common language selection dropdown (from Tim Litwiller)
-// =======================================
-function lang_dropdown()
-{
+
+
+/** Make common language selection dropdown.
+ * Dropdown corresponds to contents of install/lang.
+ * @author     Tim Litwiller */
+function lang_dropdown() {
     global $currentlang;
+
     echo "<select name=\"alanguage\" class=\"pn-text\">";
+
     $lang = languagelist();
+
+    // List install/lang
     $handle = opendir('install/lang');
     while ($f = readdir($handle)) {
         if (is_dir("install/lang/$f") && @$lang[$f]) {
             $langlist[$f] = $lang[$f];
         } 
     } 
+
     asort($langlist);
+
+    // Generation of html code
     foreach ($langlist as $k => $v) {
         echo '<option value="' . $k . '"';
+
         if ($currentlang == $k) {
             echo ' selected';
         } 
+
         echo '>' . $v . '</option> ';
     } 
     echo "</select>";
 } 
-// list of all availabe languages (from Patrick Kellum <webmaster@ctarl-ctarl.com>)
-// ==============================
-function languagelist()
-{ 
+
+
+/** List of all availabe languages.
+ * @return      array      Hash containing codes of all available lanquages 
+ * @author      Patrick Kellum <webmaster@ctarl-ctarl.com> */
+function languagelist() { 
     // All entries use ISO 639-2/T
     $lang['ara'] = _LANGUAGE_ARA; // Arabic
     $lang['bul'] = _LANGUAGE_BUL; // Bulgarian
@@ -109,12 +110,15 @@ function languagelist()
     $lang['tur'] = _LANGUAGE_TUR; // Turkish
     $lang['ukr'] = _LANGUAGE_UKR; // Ukrainian
     $lang['yid'] = _LANGUAGE_YID; // Yiddish 
+
     // Non-ISO entries are written as x_[language name]
     $lang['x_brazilian_portuguese'] = _LANGUAGE_X_BRAZILIAN_PORTUGUESE; // Brazilian Portuguese
     $lang['x_klingon'] = _LANGUAGE_X_KLINGON; // Klingon
     $lang['x_rus_koi8r'] = _LANGUAGE_X_RUS_KOI8R; // Russian KOI8-R 
     // end of list
+
     return $lang;
+
 } 
 
 ?>
