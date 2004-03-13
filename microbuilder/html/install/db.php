5a4d11698957ff21558881d163764a786fa8f424
mbertier <mbertier>
2004-03-13 04:40:39 +0000

    Comments update

--- microbuilder/html/install/db.php
+++ microbuilder/html/install/db.php
@@ -1,33 +1,14 @@
-<?php 
-// File: $Id: db.php,v 1.1 2004/03/01 10:09:13 mbertier Exp $ $Name:  $
-// ----------------------------------------------------------------------
-// POST-NUKE Content Management System
-// Copyright (C) 2001 by the Post-Nuke Development Team.
-// http://www.postnuke.com/
-// ----------------------------------------------------------------------
-// Based on:
-// PHP-NUKE Web Portal System - http://phpnuke.org/
-// Thatware - http://thatware.org/
-// ----------------------------------------------------------------------
-// LICENSE
+<?php
+/** Provide common db functions for the installer.
+ * @version      $Id: db.php,v 1.2 2004/03/13 04:40:39 mbertier Exp $ $Name:  $
+ * @package      Install
+ * @licence      GPL
+ * @author       Gregor J. Rothfuss
+ */
 
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
-// Original Author of file:  Gregor J. Rothfuss
-// Purpose of file: Provide common db functions for the installer.
-// ----------------------------------------------------------------------
-/**
- * Connect to Database
+/** Connect to Database
+ * @return    object ADONewConnection    ADODB db handle
  */
 function dbconnect($dbhost, $dbuname, $dbpass, $dbname, $dbtype = 'mysql')
 {
@@ -37,19 +18,25 @@ function dbconnect($dbhost, $dbuname, $dbpass, $dbname, $dbtype = 'mysql')
     $dbconn = &ADONewConnection($dbtype);
     $dbh = $dbconn->Connect($dbhost, $dbuname, $dbpass, $dbname);
     $ADODB_FETCH_MODE = ADODB_FETCH_NUM; 
+
     // if we get an error, log it and die
     if ($dbh === false) {
+
         error_log ("connect string: $connectString");
         error_log ("error: " . $dbconn->ErrorMsg()); 
+
         // show error and die
         PN_DBMsgError($dbconn, __FILE__ , __LINE__, "Error connecting to db");
+
     } else {
+
         return $dbconn;
+
     } 
 } 
 
-/**
- * Error message due a ADODB SQL error and die (copied from mainfile.php because it is not included
+
+/** Error message due a ADODB SQL error and die (copied from mainfile.php because it is not included
  */
 function PN_DBMsgError($db = '', $prg = '', $line = 0, $message = 'Error accessing the database')
 {
