42a432b91c04bc86122031faa216d9b821d7a8ed
mbertier <mbertier>
2004-03-19 14:45:10 +0000

    Doc update

--- microbuilder/html/config.php
+++ microbuilder/html/config.php
@@ -3,23 +3,23 @@
  * ------------------------
  * Database & System Config
  * ------------------------
- * - dbtype:     type of database, currently only mysql
- * - dbhost:     MySQL Database Hostname
- * - dbuname:    MySQL Username
- * - dbpass:     MySQL Password
- * - dbname:     MySQL Database Name
- * - system:     0 for Unix/Linux, 1 for Windows
- * - encoded:    0 for MySQL information unenccoded
- *               1 for encoded
+ *   - dbtype:     type of database, currently only mysql
+ *   - dbhost:     MySQL Database Hostname
+ *   - dbuname:    MySQL Username
+ *   - dbpass:     MySQL Password
+ *   - dbname:     MySQL Database Name
+ *   - system:     0 for Unix/Linux, 1 for Windows
+ *   - encoded:    0 for MySQL information unenccoded
+ *                 1 for encoded
  *
- * @version      $Id: config.php,v 1.3 2004/03/13 00:08:41 mbertier Exp $
+ * @version      $Id: config.php,v 1.4 2004/03/19 14:45:10 mbertier Exp $
  * @package      Installer
  * @license      GPL
  */
 
 # -- Default database settings
 $pnconfig['dbtype'] = 'mysql';
-$pnconfig['dbtabletype'] = 'MyISAM';
+$pnconfig['dbtabletype'] = 'myisam';
 $pnconfig['dbhost'] = 'localhost';
 $pnconfig['dbuname'] = '';
 $pnconfig['dbpass'] = '';
@@ -27,6 +27,7 @@ $pnconfig['dbname'] = 'Microbuilder';
 $pnconfig['system'] = '0';
 $pnconfig['prefix'] = 'mb';
 $pnconfig['encoded'] = '1';
+
 
 // ----------------------------------------------------------------------
 // For debugging (Pablo Roca)
